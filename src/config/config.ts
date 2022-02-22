ca663abbe0b2ce448803598dd061bb19c3b539a2
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-02-22 01:07:36 -0600

    fix type in database connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  NODE_ENV: process.env.ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
 };
 
